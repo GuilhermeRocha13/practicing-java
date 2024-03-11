a84da37e05dcca1f68752c77bc50ce5df42e9225
Guilherme Henrique <guilherme_h2006@hotmail.com>
2024-03-11 11:33:50 -0300

    http method

--- src/com/screenmatch/main/main_search.java
+++ src/com/screenmatch/main/main_search.java
@@ -1,2 +1,14 @@
-package com.screenmatch.main;public class main_search {
+package com.screenmatch.main;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class main_search {
+    public static void main (String[] args){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://foo.com/"))
+                .build();
+    }
 }
